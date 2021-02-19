30f80ba519aaf0bf0af5004dede383d70914795a
LSR91 <62034907+LSR91@users.noreply.github.com>
2021-02-19 19:36:43 +0100

    part1-anecdotes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Button = (props) => <button onClick={props.buttonFunction}>{props.text}</button>
+const Section = (props) => <h1>{props.text}</h1>
+
+const App = (props) => {
+
+  //Const
+  const {anecdotes} = props;
+  const {length} = anecdotes;
+  const INITIAL_VOTES=new Array(length).fill(0);
+
+  //State
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(INITIAL_VOTES)
+
+ //Random number different from previous one
+  const randomNumber=(len)=>{
+  	let rand=Math.floor(Math.random()*len);
+  	while(rand===selected){
+  		rand=Math.floor(Math.random()*len);
+  	}
+  	return rand;
+  }
+
+  //Select next random anecdote
+  const nextAnecdote = (len) => {
+  	setSelected(randomNumber(len));
+  }
+
+  //Add votes
+  const addVotes = (i) => {
+  	const copy=[...votes];
+  	copy[i]+=1;
+  	setVotes(copy);
+  }
+
+  //Most Vote
+  const mostVoted = () => {
+  	let tempMax=0, tempIndex=0;
+  	for(let i=0; i<votes.length; i++){
+  		if (votes[i]>tempMax){
+  			tempMax=votes[i];
+  			tempIndex=i;
+  		}
+  	}
+  	return tempIndex;
+  }
+
+  return (
+    <div>
+      <Section text="Anecdote of the day" />
+      {anecdotes[selected]}
+      <br />
+      has {votes[selected]} votes
+      <br />
+      <Button text="vote" buttonFunction={()=>addVotes(selected)}/>
+      <Button text="next anecdote" buttonFunction={()=>nextAnecdote(length)}/>
+      <Section text="Anecdote with most votes" />
+      {anecdotes[mostVoted()]}
+      <br />
+      has {votes[mostVoted()]} votes
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App anecdotes={anecdotes}/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
